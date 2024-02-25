ed5f103126fae7625b1bd8a300c4f9b23a267639
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-02-25 23:00:27 +0530

    feat: initial file setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+/**
+ * © Aralam Farm 2024. All rights reserved.
+ *
+ * This code is the property of Aralam Farm and is protected by copyright law.
+ * Unauthorized use, reproduction, or distribution is strictly prohibited.
+ *
+ * @author Janajith D
+ */
+
+import React from "react";
+import type { Metadata } from "next";
+import { Children } from "@lib/types";
+import "@styles/scss/main.scss";
+
+export const metadata: Metadata = {
+  title: "Aralam Farm",
+  description:
+    "Aralam Farm and Wildlife Sanctuary is evergreen and delicious forest area spanning 55kms located on the western slope of the Western Ghats",
+};
+
+export default function RootLayout({ children }: Children) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/common/components/Link.tsx
@@ -0,0 +1,11 @@
+/**
+ * © Aralam Farm 2024. All rights reserved.
+ *
+ * This code is the property of Aralam Farm and is protected by copyright law.
+ * Unauthorized use, reproduction, or distribution is strictly prohibited.
+ *
+ * @author Janajith D
+ */
+
+import Link from "next/link";
+export default Link;
