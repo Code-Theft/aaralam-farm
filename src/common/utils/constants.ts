f789040545af2d992a7823abb3cf537cc21aeb4c
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-05-19 14:59:03 +0530

    feat: added navbar desktop only

--- src/common/utils/constants.ts
+++ src/common/utils/constants.ts
@@ -1,4 +1,19 @@
 export const imageAlts = {
-    HOME: "Aaralam Farm"
-}
+  HOME: "Aaralam Farm",
+};
 
+export const pageRoutes = {
+  HOME: "/",
+  PLANTS: "/plants",
+  PRODUCTS: "/products",
+  ABOUT: "/about",
+  TOURISM: "/tourism",
+};
+
+export const navbarLinks = {
+  HOME: "home",
+  PLANTS: "plants",
+  PRODUCTS: "products",
+  ABOUT: "about",
+  TOURISM: "tourism",
+};
--- src/widgets/Header/view.tsx
+++ src/widgets/Header/view.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+"use client";
+
+import React, { useMemo } from "react";
 import Link from "@components/Link";
 import AralamLogo from "@core/AralamLogo";
 import IconChevronDown from "@icons/IconChevronDown";
+import { usePathname } from "next/navigation";
+import { navbarLinks, pageRoutes } from "@utils/constants";
 
 export default function HeaderView() {
+  const pathname = usePathname();
+  const navbarItems = useMemo(() => {
+    const navLinks = [
+      {
+        id: navbarLinks.HOME,
+        path: "/",
+        label: "Home",
+      },
+      {
+        id: navbarLinks.PRODUCTS,
+        path: "/products",
+        label: "Farm Products",
+      },
+      {
+        id: navbarLinks.PLANTS,
+        path: "/plants",
+        label: "Farm Plants",
+      },
+      {
+        id: navbarLinks.TOURISM,
+        path: "/tourism",
+        label: "Farm Tourism",
+      },
+    ];
+    return navLinks.map((item) => {
+      const isActiveLink =
+        (pathname === pageRoutes.HOME && item?.id === navbarLinks.HOME) ||
+        (item?.path.includes(pathname) && pathname !== pageRoutes.HOME);
+      // (pathname.includes(pageRoutes) && item?.path === pageRoutes.ORDER);
+
+      return { ...item, active: isActiveLink };
+    });
+  }, [pathname]);
+
   return (
     <header>
       <div className="header-top">
@@ -14,9 +52,14 @@ export default function HeaderView() {
             </Link>
           </div>
           <div className="nav-links d-none d-md-flex">
-            <div>
-              <Link href="/">Home</Link>
-            </div>
+            {navbarItems?.map((item) => (
+              <div
+                className={`links${item?.active ? " active" : ""}`}
+                key={item?.id}
+              >
+                <Link href={item?.path}>{item?.label}</Link>
+              </div>
+            ))}
           </div>
           <div className="mobile-nav-links d-flex d-md-none">
             <IconChevronDown />
